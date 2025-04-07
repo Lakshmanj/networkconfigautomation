528c2ed6930e2d6288fa9c19e095719767df895c
ABeanIRL <145500140+ABeanIRL@users.noreply.github.com>
2025-04-06 21:01:00 -0400

    Updated Python File with explanations

--- network_config.py
+++ network_config.py
@@ -1,12 +1,40 @@
 from netmiko import ConnectHandler
+# ConnectHandler allows for SSH connections to the network devices
 
+# Create a python dictionary with will contain the necessary details to connect to the device
+# if we're connecting to multiple devices, make multiple dictionaries
 device = {
-    'device_type': '',
-    'host': '192.168.56.101',
-    'username': '',
+    'device_type': '',  # This specifies the type of device we're connecting to and helps
+                        # Netmiko know how to handle said connection
+    'host': '',         # This is the ip address/hostname of the device we're connecting to
+    'username': '',     # Username and password to connect to the device
     'password': '',
-    'secret': '',
 }
-
+# Connection is a variable that stores the "ConnectHandler" function
+# it will unpack the dictionary(ies) and establish the connection(s)
 connection = ConnectHandler(**device)
-connection.enable()
+
+# if connecting to multiple devices use this;
+# for device in (device1, device2, device3... device#:
+#    net_connect = ConnectHandler(**device)
+
+connection.enable()     # This allows the device to enter "enabled" mode, which will allow us to perform
+                        # the certian configuration tasks for said device(s)
+                        #if the device requires an password, it'll be used here
+
+# Below is a list which will perform the config commands specified inside and will apply to said device(s)
+config_commands = [
+    '' # Put whatever commands here
+]
+
+# This will send the list, "config_commands" to the device through the established SSH session
+# using ".send_config_set" will send multiple commands at once to the device and waits for the output
+# the "output" variable will capture the response and store it
+output = connection.send_config_set(config_commands)
+
+# Will print out the result of the command execution returned by
+# the device whether it be a success, failure, error, or the results of the commands
+print(output)
+
+# This will disconnect the session
+connection.disconnect()
